ec61a65dc148fb8fed0e1712c8cedd870962df67
ToxicStar <429771027@qq.com>
2023-08-18 15:55:58 +0800

    init

---
+++ EmptyMono.cs
@@ -0,0 +1,16 @@
+/*********************************************
+ * BFramework
+ * 空Mono
+ * 创建时间：2023/08/16 17:00:23
+ *********************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+    public class EmtpyMono : MonoBehaviour
+    {
+
+    }
+}
